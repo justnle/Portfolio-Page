5a279b921f3c2106ca56ca03859c1a67256aad1c
Justin <twoplatinumchainz@gmail.com>
2020-01-25 01:56:43 -0800

    removed unrequired script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,1 @@
-$('body').scrollspy({ target: '.navbar', offset: 50 });
 
-$('#top-nav a, #navigation-info-container a').on('click', function(event) {
-  if (this.hash !== '') {
-    event.preventDefault();
-
-    var hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      500,
-      function() {
-        window.location.hash = hash;
-      }
-    );
-  }
-});
